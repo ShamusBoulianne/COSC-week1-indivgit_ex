f522e342fb642c8bd40136a407c93e83cc3141e1
SlimShamy <43048215+SlimShamy@users.noreply.github.com>
2020-09-15 19:53:09 -0700

    added revIntSort() method

--- sorting.py
+++ sorting.py
@@ -4,11 +4,19 @@ def intSort(x):
     x.sort()
     return x
 
+def revIntSort(x):
+    x.sort(reverse=True)
+    return x
+
 class testSorting(unittest.TestCase):
     
     def test_intSort(self):
         x = [3, 4, 2, 1]
         self.assertTrue(intSort(x) == [1, 2, 3, 4])
+        
+    def test_revIntSort(self):
+        x = [3, 4, 2, 1]
+        self.assertTrue(revIntSort(x) == [4, 3, 2, 1])
         
 if __name__ == '__main__':
     unittest.main()
